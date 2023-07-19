e0a8762f67b6a024ad67c366c3ddf7dd66f73a0f
jazmin <jazmasoterapeuta@gmail.com>
2023-07-18 22:52:14 -0400

    firebase datos

--- my-app/src/firebase/firebase.js
+++ my-app/src/firebase/firebase.js
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -6,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB84GoA4NNOX2_N9wOkwpyPNVucfvrgCTg",
-  authDomain: "burger-queen-1371f.firebaseapp.com",
-  projectId: "burger-queen-1371f",
-  storageBucket: "burger-queen-1371f.appspot.com",
-  messagingSenderId: "664247609865",
-  appId: "1:664247609865:web:8a470b54e94cc823d9a6d8"
+  apiKey: "AIzaSyBf5_uiA98oJpcprlp8Ekzcu6SmM5a2cRc",
+  authDomain: "my-burger-8fab9.firebaseapp.com",
+  projectId: "my-burger-8fab9",
+  storageBucket: "my-burger-8fab9.appspot.com",
+  messagingSenderId: "921842014942",
+  appId: "1:921842014942:web:c4ba21770945b4e7f9404f"
 };
 
 // Initialize Firebase
